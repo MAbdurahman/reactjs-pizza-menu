c3307ef8a16b12f24c9a221035be768897453c36
MAbdurahman <mdbdrrhm@gmail.com>
2023-09-08 06:52:05 -0500

    Add pizzaData.js and default styles in index.css

---
+++ src/pizzaData.js
@@ -0,0 +1,45 @@
+
+export const pizzaData = [
+    {
+        name: "Focaccia",
+        ingredients: "Bread with italian olive oil and rosemary",
+        price: 6,
+        photoName: "assets/focaccia.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Margherita Pizza",
+        ingredients: "Tomato and mozzarella",
+        price: 10,
+        photoName: "assets/margherita.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Spinach Pizza",
+        ingredients: "Tomato, mozzarella, spinach, and ricotta cheese",
+        price: 12,
+        photoName: "assets/spinach.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Mushroom Pizza",
+        ingredients: "Tomato, mozzarella, mushrooms, and onion",
+        price: 12,
+        photoName: "assets/mushrooms.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pepperoni Pizza",
+        ingredients: "Tomato, mozzarella, and pepperoni",
+        price: 15,
+        photoName: "assets/pepperoni.jpg",
+        soldOut: true,
+    },
+    {
+        name: "Pizza Prosciutto",
+        ingredients: "Tomato, mozzarella, ham, arugula, and burrata cheese",
+        price: 18,
+        photoName: "assets/prosciutto.jpg",
+        soldOut: false,
+    },
+]
